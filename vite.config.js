0e5806790946535fd7ae050c5585d5926097f49e
snixie <fsd.rabgay@gmail.com>
2023-03-27 03:05:59 +0600

    cors.js config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import corsMiddleware from "./cors";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    middleware: {
+      custom: corsMiddleware,
+    },
+  },
 });
