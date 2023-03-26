2467168ee67949f052d0c79d7daed0841f486716
snixie <fsd.rabgay@gmail.com>
2023-03-27 03:21:15 +0600

    rm cor.js config

--- cors.js
+++
@@ -1,9 +0,0 @@
-export default function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
-  next();
-}
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import corsMiddleware from "./cors";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    middleware: {
-      custom: corsMiddleware,
-    },
-  },
 });
