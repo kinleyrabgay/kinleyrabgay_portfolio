e339a2c73b4e3b288c74b9b727bdb805e8dfbb20
snixie <fsd.rabgay@gmail.com>
2023-03-27 19:17:46 +0600

    profile updates

--- src/constants/images.js
+++ src/constants/images.js
@@ -23,7 +23,7 @@ import about02 from "../assets/about02.png";
 import about03 from "../assets/about03.png";
 import about04 from "../assets/about04.png";
 
-import profile from "../assets/profile.png";
+import demo_profile from "../assets/demo_profile.png";
 import circle from "../assets/circle.svg";
 import logo from "../assets/logo.png";
 
@@ -34,7 +34,6 @@ import bolt from "../assets/bolt.png";
 import nb from "../assets/nb.png";
 import skype from "../assets/skype.png";
 import spotify from "../assets/spotify.png";
-import portfolio from "../assets/portfolio.png";
 
 export default {
   email,
@@ -60,7 +59,6 @@ export default {
   about02,
   about03,
   about04,
-  profile,
   circle,
   logo,
   adidas,
@@ -70,5 +68,5 @@ export default {
   nb,
   skype,
   spotify,
-  portfolio,
+  demo_profile,
 };
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -46,7 +46,7 @@ const Header = () => {
         transition={{ duration: 0.5, delayChildren: 0.5 }}
         className="app__header-img"
       >
-        <img src={images.profile} alt="profile_bg" />
+        <img src={images.demo_profile} alt="profile_bg" />
         <motion.img
           whileInView={{ scale: [0, 1] }}
           transition={{ duration: 1, ease: "easeInOut" }}
