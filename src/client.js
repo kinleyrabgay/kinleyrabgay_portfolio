4a7358e5f1670fdfe1f54216abdff7d439a0cbda
snixie <fsd.rabgay@gmail.com>
2023-05-28 00:55:55 +0600

    updated content

--- src/client.js
+++ src/client.js
@@ -1,10 +1,10 @@
-import sanityClient from "@sanity/client";
-import imageUrlBuilder from "@sanity/image-url";
+import sanityClient from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
   projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
-  dataset: "production",
-  apiVersion: "2022-02-01",
+  dataset: 'production',
+  apiVersion: '2022-02-01',
   useCdn: true,
   token: import.meta.env.VITE_SANITY_TOKEN,
 });
